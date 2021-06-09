1b8bee49f67e50c4849a0a95b617e83c1aeb40cf
Nicolas Esteban <nicolas.esteban@cirvianum.cat>
2021-06-09 14:26:12 +0200

    1

---
+++ nicolas/WEB-INF/classes/nicolas.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class nicolas extends HttpServlet {
+
+   private char[] surname = new char[]{ 'e', 's', 't', 'e', 'b', 'a', 'n' };
+
+   public void init() throws ServletException { }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      for (char letter : surname) {
+	out.println(letter);
+      }
+   }
+
+   public void destroy() { }
+}
